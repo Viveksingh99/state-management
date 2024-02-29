c85e385facb57cc37aa83c119aee1a6a73616d69
Viveksingh99 <vivek22jan99@gmail.com>
2024-03-01 00:41:06 +0530

    learn Redux Toolkit and make simple Todo with Redux-Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
       <Login />
       <Profile />
+      <AddTodo/>
+      <Todos/>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { UserContextProvider } from './context/MyContextProvider.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { UserContextProvider } from "./context/MyContextProvider.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/Store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <UserContextProvider>
-    <App />
+      <App />
     </UserContextProvider>
-  </React.StrictMode>,
-)
+  </Provider>
+);
